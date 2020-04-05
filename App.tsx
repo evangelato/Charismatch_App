2717e215a48d99fa959f95295c755df3abfa1a1e
Evan Hong <evangelurtz@gmail.com>
2020-04-05 22:58:47 +0900

    Remove Shoutem UI and add UI Kitten

--- App.tsx
+++ App.tsx
@@ -1,39 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { AppLoading } from "expo";
-import * as Font from "expo-font";
 import { StatusBar } from "react-native";
-import { View, Examples } from "@shoutem/ui";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
+import { mapping, light as lightTheme } from "@eva-design/eva";
+
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text category="h1">HOME</Text>
+  </Layout>
+);
 
 const App: React.FC = () => {
   const [fontLoaded, setFontLoaded] = useState(false);
   useEffect(() => {
-    const loadFonts = async (): Promise<void> => {
-      await Font.loadAsync({
-        "Rubik-Black": require("./node_modules/@shoutem/ui/fonts/Rubik-Black.ttf"),
-        "Rubik-BlackItalic": require("./node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf"),
-        "Rubik-Bold": require("./node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf"),
-        "Rubik-BoldItalic": require("./node_modules/@shoutem/ui/fonts/Rubik-BoldItalic.ttf"),
-        "Rubik-Italic": require("./node_modules/@shoutem/ui/fonts/Rubik-Italic.ttf"),
-        "Rubik-Light": require("./node_modules/@shoutem/ui/fonts/Rubik-Light.ttf"),
-        "Rubik-LightItalic": require("./node_modules/@shoutem/ui/fonts/Rubik-LightItalic.ttf"),
-        "Rubik-Medium": require("./node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf"),
-        "Rubik-MediumItalic": require("./node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf"),
-        "Rubik-Regular": require("./node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf"),
-        "rubicon-icon-font": require("./node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf")
-      });
-      setFontLoaded(true);
-    };
-    loadFonts();
+    return;
   }, []);
-
-  if (!fontLoaded) {
-    return <AppLoading />;
-  }
   return (
-    <View styleName="flexible">
-      <Examples />
-      <StatusBar barStyle="default" hidden={false} />
-    </View>
+    <ApplicationProvider mapping={mapping} theme={lightTheme}>
+      <HomeScreen />
+    </ApplicationProvider>
   );
 };
 
