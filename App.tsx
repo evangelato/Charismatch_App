e6f8462b24cb56b16001fc238c0b118853bc7a85
Evan Hong <evangelurtz@gmail.com>
2020-04-06 16:56:51 +0900

    Add react navigation

--- App.tsx
+++ App.tsx
@@ -3,12 +3,17 @@ import { AppLoading } from "expo";
 import { StatusBar } from "react-native";
 import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
 import { mapping, light as lightTheme } from "@eva-design/eva";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Loign from "./components/Login";
 
 const HomeScreen = () => (
   <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Text category="h1">HOME</Text>
   </Layout>
 );
+
+const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   const [fontLoaded, setFontLoaded] = useState(false);
@@ -17,7 +22,12 @@ const App: React.FC = () => {
   }, []);
   return (
     <ApplicationProvider mapping={mapping} theme={lightTheme}>
-      <HomeScreen />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={Loign} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ApplicationProvider>
   );
 };
--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Layout, Text, Button } from "@ui-kitten/components";
+//import { StackNavigationProp } from "@react-navigation/stack";
+import { StackNavigationProp } from "@react-navigation/stack";
+import PropTypes from "prop-types";
 
-const Login: React.FC = () => {
-  return <React.Fragment />;
+interface Props {
+  navigation: any;
+}
+
+const Login: React.FC<Props> = ({ navigation }) => {
+  return (
+    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text category="h1">LOGIN</Text>
+      <Button onPress={() => navigation.navigate("Home")}>Login</Button>
+    </Layout>
+  );
+};
+
+Login.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired
+  }).isRequired
 };
 
 export default Login;
