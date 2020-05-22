c8a75a5e846cc6a07f7692c6d85d07b2c4fbe2d4
Evan Hong <evangelurtz@gmail.com>
2020-05-22 20:22:19 +0900

    Fix test server url

--- src/config/const.tsx
+++ src/config/const.tsx
@@ -4,10 +4,12 @@ const { manifest } = Constants;
 
 export const api =
   typeof manifest.packagerOpts === `object` && manifest.packagerOpts.dev
-    ? manifest
-        .debuggerHost!.split(`:`)!
-        .shift()!
-        .concat(`:4000/api`)
+    ? 'http://'.concat(
+        manifest
+          .debuggerHost!.split(`:`)!
+          .shift()!
+          .concat(`:4000/api`),
+      )
     : `api.example.com`;
 
 export const baseUrl = process.env.NODE_ENV === 'production' ? '' : api;
